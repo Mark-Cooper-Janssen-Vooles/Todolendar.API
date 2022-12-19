f0105839613ed9201e5be07fe4744d1d8e537ace
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2022-12-19 12:22:46 +1100

    dependency inject db context

--- Todolender.API/Todolender.API/Data/TodolenderDbContext.cs
+++ Todolender.API/Todolender.API/Data/TodolenderDbContext.cs
@@ -1,6 +1,17 @@
-﻿namespace Todolender.API.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Todolender.API.Models.Domain;
+
+namespace Todolender.API.Data
 {
-    public class TodolenderDbContext
+    public class TodolenderDbContext : DbContext
     {
+        public TodolenderDbContext(DbContextOptions<TodolenderDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<PlanReminder> PlanReminder { get; set; }
+        public DbSet<ScheduledTodo> ScheduledTodo { get; set; }
+        public DbSet<ScheduledTodoChild> ScheduledTodoChild { get; set;}
+        public DbSet<Todo> Todos { get; set; }
+        public DbSet<TodoList> TodosList { get; set;}
     }
 }
