3141341edce8818737308b4f3fd2e0f77003884f
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2022-12-20 17:55:25 +1100

    [MASTER] Create DTOand profile

---
+++ Todolender.API/Todolender.API/Models/Profiles/PlanReminderProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Todolender.API.Models.Domain;
+using Todolender.API.Models.DTO;
+
+namespace Todolender.API.Models.Profiles
+{
+    public class PlanReminderProfile : Profile
+    {
+        public PlanReminderProfile()
+        {
+            CreateMap<PlanReminder, PlanReminderDTO>().ReverseMap();
+        }
+    }
+}
