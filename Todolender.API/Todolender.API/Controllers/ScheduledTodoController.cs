ad5b788fa01c2ec5e6716b7c58b36efdf8242aa1
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2022-12-20 11:39:59 +1100

    Create scheduledtodo controller and createscheduledtodorequest DTO

---
+++ Todolender.API/Todolender.API/Controllers/ScheduledTodoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Todolender.API.Models.DTO;
+
+namespace Todolender.API.Controllers
+{
+    [ApiController]
+    [Route("ScheduledTodo")]
+    public class ScheduledTodoController
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateScheduledTodoAsync(CreateScheduledTodoRequest createScheduledTodoRequest)
+        {
+            // validations 
+
+            // map DTO to domain 
+
+            // use repository to create and return scheduled todo domain model
+
+            // map domain model to DTO 
+
+            // return status to user with DTO
+
+            // put auth in 
+        }
+    }
+}
---
+++ Todolender.API/Todolender.API/Models/DTO/CreateScheduledTodoRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace Todolender.API.Models.DTO
+{
+    public class CreateScheduledTodoRequest
+    {
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public string Colour { get; set; }
+        public bool Active { get; set; }
+        public int RecurCount { get; set; }
+        public string RecurFrequencyType { get; set; }
+        public DateTime RecurEndDate { get; set; }
+        public int NotifyBeforeTime { get; set; }
+    }
+}
