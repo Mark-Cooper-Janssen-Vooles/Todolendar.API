2e4719324b8c599c847d22347cfdba0e95d1e4d0
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2022-12-20 13:32:09 +1100

    [MASTER] Add validations

---
+++ Todolender.API/Todolender.API/Validators/UpdateScheduledTodoRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Todolender.API.Models.DTO;
+
+namespace Todolender.API.Validators
+{
+    public class UpdateScheduledTodoRequestValidator : AbstractValidator<UpdateScheduledTodoRequest>
+    {
+        public UpdateScheduledTodoRequestValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty();
+            RuleFor(x => x.Colour).NotEmpty();
+            RuleFor(x => x.RecurCount).GreaterThanOrEqualTo(0);
+            RuleFor(x => x.RecurFrequencyType).NotEmpty();
+            RuleFor(x => x.RecurEndDate).NotNull();
+            RuleFor(x => x.NotifyBeforeTime).GreaterThanOrEqualTo(5);
+        }
+    }
+}
