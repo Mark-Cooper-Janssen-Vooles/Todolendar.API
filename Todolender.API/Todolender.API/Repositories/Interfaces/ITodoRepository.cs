71e4ceabaa24223baf8e4ab42b849be23f9ab306
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2022-12-21 12:27:13 +1100

    [MASTER] Create ITodoRepository

---
+++ Todolender.API/Todolender.API/Repositories/Interfaces/ITodoRepository.cs
@@ -0,0 +1,9 @@
+﻿using Todolender.API.Models.Domain;
+
+namespace Todolender.API.Repositories.Interfaces
+{
+    public interface ITodoRepository
+    {
+        Task<Todo> CreateTodoAsync(Guid userId, Todo todo);
+    }
+}
