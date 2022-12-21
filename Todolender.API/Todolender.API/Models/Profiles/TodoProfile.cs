ddec1b4468608c68e9acbb3c59432ac5995bdaae
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2022-12-21 12:25:45 +1100

    [MASTER] Add todoprofile for mapper

---
+++ Todolender.API/Todolender.API/Models/Profiles/TodoProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Todolender.API.Models.Domain;
+using Todolender.API.Models.DTO.Auth;
+using Todolender.API.Models.DTO.Todo;
+
+namespace Todolender.API.Models.Profiles
+{
+    public class TodoProfile : Profile
+    {
+        public TodoProfile()
+        {
+            CreateMap<Todo, CreateTodoRequest>().ReverseMap();
+        }
+    }
+}
