5f3fa866413c96598f7015176fef2cabe60870cd
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-03-10 15:08:11 +1100

    [MASTER] Add migration after adding enum

--- Todolendar.API/Todolendar.API/Migrations/20230310040545_use entity for scheduledTodo.Designer.cs
+++ Todolendar.API/Todolendar.API/Migrations/20230310040545_use entity for scheduledTodo.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Todolendar.API.Data;
 
@@ -11,9 +12,11 @@ using Todolendar.API.Data;
 namespace Todolendar.API.Migrations
 {
     [DbContext(typeof(TodolendarDbContext))]
-    partial class TodolenderDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230310040545_use entity for scheduledTodo")]
+    partial class useentityforscheduledTodo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
