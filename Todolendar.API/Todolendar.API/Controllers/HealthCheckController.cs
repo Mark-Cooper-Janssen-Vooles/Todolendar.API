3fd018bf75cffacd231d0e1b2c08536c08cfc485
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-03-22 17:25:56 +1100

    Add health check

---
+++ Todolendar.API/Todolendar.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Todolendar.API.Models.DTO.Auth;
+using Todolendar.API.Repositories.Interfaces;
+
+namespace Todolendar.API.Controllers
+{
+    [ApiController]
+    [Route("Ping")]
+    public class HealthCheckController : ControllerBase
+    {
+        public HealthCheckController() {}
+
+        [HttpGet]
+        public async Task<IActionResult> healthCheck()
+        {
+            return Ok();
+        }
+    }
+}
