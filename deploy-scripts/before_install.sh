4c2665b693c5875b8a057f707930c4a38ed9657e
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-03-23 19:25:30 +1100

    add echo statements

--- deploy-scripts/before_install.sh
+++ deploy-scripts/before_install.sh
@@ -1,18 +1,21 @@
 # guide followed here: https://www.bluelabellabs.com/blog/how-to-setup-aws-codepipeline-for-a-asp-net-core-web-api-project/
 
 # install nvm
+echo "installing nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 . ~/.nvm/nvm.sh
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
-
+echo "installing node 16"
 nvm install 16
 
 # install PM2 (process management)
-npm install pm2 -g
+echo "installing pm2"
+npm install pm2
 
 # install .net runtime - https://learn.microsoft.com/en-us/dotnet/core/install/linux-scripted-manual#scripted-install
+echo "installing .net runtime"
 sudo wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 sudo chmod +x ./dotnet-install.sh
 ./dotnet-install.sh --version latest --runtime dotnet
