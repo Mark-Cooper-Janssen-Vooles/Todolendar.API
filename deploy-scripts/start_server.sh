7c8265bb46bcbb12a538f060f4c2882b27c622d7
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-03-23 19:58:00 +1100

    Change start server script

--- deploy-scripts/start_server.sh
+++ deploy-scripts/start_server.sh
@@ -1,2 +1,6 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 cd /home/ec2-user/api/Todolendar.API/publish
 pm2 start Todolendar.API.dll
