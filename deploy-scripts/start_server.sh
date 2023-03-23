8f916acb9db16166d3bf062b11f82a2d3aa78189
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-03-23 19:58:56 +1100

    confirm nvm installed

--- deploy-scripts/start_server.sh
+++ deploy-scripts/start_server.sh
@@ -2,5 +2,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
+echo "nvm ls"
+echo "npm -v" 
+
 cd /home/ec2-user/api/Todolendar.API/publish
 pm2 start Todolendar.API.dll
