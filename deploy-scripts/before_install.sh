dac5a1d3d6544bfea130ab17d1541f31903e47ee
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-03-23 16:54:52 +1100

    update script

--- deploy-scripts/before_install.sh
+++ deploy-scripts/before_install.sh
@@ -3,12 +3,14 @@
 # install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 . ~/.nvm/nvm.sh
+
+
 nvm install 16
 
 # install PM2 (process management)
 npm install pm2 -g
 
 # install .net runtime - https://learn.microsoft.com/en-us/dotnet/core/install/linux-scripted-manual#scripted-install
-wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
+sudo wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 sudo chmod +x ./dotnet-install.sh
 ./dotnet-install.sh --version latest --runtime dotnet
