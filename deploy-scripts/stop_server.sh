8e369aafdcea6e9c0c1934abf5179278ae5e16d2
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-03-23 20:14:37 +1100

    rename stop command

--- deploy-scripts/stop_server.sh
+++ deploy-scripts/stop_server.sh
@@ -2,5 +2,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-pm2 stop Todolendar.API
-pm2 delete Todolendar.API
+pm2 stop todolender
+pm2 delete todolender
