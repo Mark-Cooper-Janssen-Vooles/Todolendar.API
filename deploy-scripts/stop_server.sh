899eeb8c6eb91c721f9659713c28f7a5e9ac0913
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-03-23 20:24:08 +1100

    fix typo

--- deploy-scripts/stop_server.sh
+++ deploy-scripts/stop_server.sh
@@ -2,5 +2,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-pm2 stop todolender
-pm2 delete todolender
+pm2 stop todolendar
+pm2 delete todolendar
