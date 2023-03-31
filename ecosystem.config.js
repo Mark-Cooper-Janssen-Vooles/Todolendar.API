389af2777ca0abf1797694cc991fe2be62d7ff18
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-03-31 17:00:22 +1100

    fix formatting

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name   : "Todolendar API",
     exec_mode: "cluster",
     env: {
-      "NODE_ENV": "production"
+      NODE_ENV: "production"
     },
     script : "sudo /home/ec2-user/dotnet/dotnet Todolendar.API.dll --urls \"http://*:5000;https://*5001\"",
     cwd: "/home/ec2-user/api/Todolendar.API/publish",
