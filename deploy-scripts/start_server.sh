3f5c2b392a07f74c9d93efad6ed0b99fb27379c2
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-03-31 14:28:25 +1100

    fix the start server script

--- deploy-scripts/start_server.sh
+++ deploy-scripts/start_server.sh
@@ -2,9 +2,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-export DOTNET_ROOT=$HOME/dotnet
-export PATH=$PATH:$HOME/dotnet
+# export DOTNET_ROOT=$HOME/dotnet
+# export PATH=$PATH:$HOME/dotnet
 
 cd /home/ec2-user/api/Todolendar.API/publish
 
-#sudo dotnet Todolendar.API.dll --urls http://0.0.0.0:80
+dotnet Todolendar.API.dll --urls "http://*:5000;https://*5001"
