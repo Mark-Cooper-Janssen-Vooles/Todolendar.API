7c71778cd25ac3496c63d4b301bedc8ebdd2ad6e
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-03-31 14:33:29 +1100

    Fix script

--- deploy-scripts/start_server.sh
+++ deploy-scripts/start_server.sh
@@ -2,8 +2,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-# export DOTNET_ROOT=$HOME/dotnet
-# export PATH=$PATH:$HOME/dotnet
+export DOTNET_ROOT=$HOME/dotnet
+export PATH=$PATH:$HOME/dotnet
 
 cd /home/ec2-user/api/Todolendar.API/publish
 
