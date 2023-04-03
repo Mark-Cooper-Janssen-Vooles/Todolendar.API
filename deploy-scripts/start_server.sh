5c6d93b9cd127da8ea4b2df5930c5114e1cc04bb
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-04-03 17:32:54 +1000

    Add env variable

--- deploy-scripts/start_server.sh
+++ deploy-scripts/start_server.sh
@@ -7,4 +7,5 @@ export PATH=$PATH:$HOME/dotnet
 
 cd /home/ec2-user/api/Todolendar.API/publish
 
+export ASPNETCORE_ENVIRONMENT=Production
 pm2 start dotnet --name todolendar -- Todolendar.API.dll --urls "http://*:5000;https://*:5001" --daemon
