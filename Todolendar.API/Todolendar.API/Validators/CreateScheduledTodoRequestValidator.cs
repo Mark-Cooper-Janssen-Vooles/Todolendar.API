d5bb070044855ad467bc246e461093ed700af48e
Mark Janssen-vooles <m.janssen-vooles@outlook.com.au>
2023-05-31 19:17:11 +1000

    Max children can be 5

--- Todolendar.API/Todolendar.API/Validators/CreateScheduledTodoRequestValidator.cs
+++ Todolendar.API/Todolendar.API/Validators/CreateScheduledTodoRequestValidator.cs
@@ -9,7 +9,7 @@ namespace Todolendar.API.Validators
         {
             RuleFor(x => x.Title).NotEmpty();
             RuleFor(x => x.Colour).NotEmpty();
-            RuleFor(x => x.RecurCount).GreaterThanOrEqualTo(0);
+            RuleFor(x => x.RecurCount).GreaterThanOrEqualTo(0).LessThanOrEqualTo(5);
             RuleFor(x => x.RecurFrequencyType).NotEmpty();
             RuleFor(x => x.RecurEndDate).NotNull();
             RuleFor(x => x.NotifyBeforeTime).GreaterThanOrEqualTo(5);
